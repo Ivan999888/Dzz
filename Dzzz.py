2f01a22ed4f7b7c5b5a8179a0fdac2a402b8a644
“Ivan” <ivan.alexandrov1998@mail.ru>
2023-05-23 14:07:19 +0300

    Added task 36

--- Dzzz.py
+++ Dzzz.py
@@ -4,22 +4,40 @@
 # Фраза может состоять из одного слова, если во фразе несколько слов, то они разделяются дефисами. Фразы отделяются друг от друга пробелами. 
 # Стихотворение Винни-Пух вбивает в программу с клавиатуры. В ответе напишите “Парам пам-пам”, если с ритмом все в порядке и “Пам парам”, если с ритмом все не в порядке
 
-def check_rhythm(poem):
-    lines = poem.split()
-    syllables_count = None
+# def check_rhythm(poem):
+#     lines = poem.split()
+#     syllables_count = None
 
-    for line in lines:
-        words = line.split('-')
-        current_syllables_count = sum([word.count('а') + word.count('е') + word.count('ё') + word.count('и') + word.count('о') + word.count('у') + word.count('ы') + word.count('э') + word.count('ю') + word.count('я') for word in words])
+#     for line in lines:
+#         words = line.split('-')
+#         current_syllables_count = sum([word.count('а') + word.count('е') + word.count('ё') + word.count('и') + word.count('о') + word.count('у') + word.count('ы') + word.count('э') + word.count('ю') + word.count('я') for word in words])
 
-        if syllables_count is None:
-            syllables_count = current_syllables_count
-        elif current_syllables_count != syllables_count:
-            return 'Пам парам'
+#         if syllables_count is None:
+#             syllables_count = current_syllables_count
+#         elif current_syllables_count != syllables_count:
+#             return 'Пам парам'
 
-    return 'Парам пам-пам'
+#     return 'Парам пам-пам'
 
 
-poem = input('Введите стихотворение: ')
-result = check_rhythm(poem)
-print(result)
+# poem = input('Введите стихотворение: ')
+# result = check_rhythm(poem)
+# print(result)
+
+# Задача 36:
+# На вход программы поступает строка в формате:
+# ключ_1=значение_1 ключ_2=значение_2 ... ключ_N=значение_N
+# Необходимо с помощью функции map преобразовать ее в кортеж tp вида:
+# tp = (('ключ_1', 'значение_1'), ('ключ_2', 'значение_2'), ..., ('ключ_N', 'значение_N'))
+# Выводить на экран ничего не нужно, только преобразовать строку в кортеж с именем tp.
+
+# Sample Input:
+# house=дом car=машина men=человек tree=дерево
+# Sample Output:
+# (('house', 'дом'), ('car','машина'), ('men', 'человек'), ('tree', 'дерево'))
+
+string = "house=дом car=машина men=человек tree=дерево"
+
+tp = tuple(map(lambda pair: tuple(pair.split('=')), string.split()))
+
+print(tp)

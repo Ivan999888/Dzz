b644ab9118a681f22c6a4dfba618a5aa1af425df
“Ivan” <ivan.alexandrov1998@mail.ru>
2023-05-23 14:11:03 +0300

    Added additionally task

--- Dzzz.py
+++ Dzzz.py
@@ -36,8 +36,32 @@
 # Sample Output:
 # (('house', 'дом'), ('car','машина'), ('men', 'человек'), ('tree', 'дерево'))
 
-string = "house=дом car=машина men=человек tree=дерево"
+# string = "house=дом car=машина men=человек tree=дерево"
 
-tp = tuple(map(lambda pair: tuple(pair.split('=')), string.split()))
+# tp = tuple(map(lambda pair: tuple(pair.split('=')), string.split()))
 
-print(tp)
+# print(tp)
+
+# Дополнительное
+# Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например, у операции умножения.
+
+# *Пример:*
+
+# **Ввод:** `print_operation_table(lambda x, y: x * y) ` 
+# **Вывод:**
+# 1 2 3 4 5 6
+
+# 2 4 6 8 10 12
+# 3 6 9 12 15 18
+# 4 8 12 16 20 24
+# 5 10 15 20 25 30
+# 6 12 18 24 30 36
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for row in range(1, num_rows + 1):
+        for col in range(1, num_columns + 1):
+            result = operation(row, col)
+            print(result, end=' ')
+        print()
+
+print_operation_table(lambda x, y: x * y)
